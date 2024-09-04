e22efc4738b7450dbe5936bfe02d7fb55d4ac1c5
Nyandieka <einsteinmokua100@gmail.com>
2024-09-04 07:03:04 +0300

    Completes test

--- server/models.py
+++ server/models.py
@@ -1,14 +1,29 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+# Initialize SQLAlchemy
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Message(db.Model, SerializerMixin):
+class Message(db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Automatically set to current time on creation
+
+    def __init__(self, body, username):
+        self.body = body
+        self.username = username
+
+    def __repr__(self):
+        return f'<Message {self.body}, {self.username}>'
+
+    def to_dict(self):
+        """Convert the Message object to a dictionary."""
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat()  # Convert datetime to ISO 8601 format
+        }
